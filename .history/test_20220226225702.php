0ec565228090fcfb67342ad149acde71108ce2b2
Slime <guenvesley@gmail.com>
2022-02-26 23:32:39 +0700

    test tap 2

---
+++ .history/test_20220226225702.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Welcome to Page</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+        background-image: url("https://maxcdn.bootstrapcdn.com/bootstrap/www.google.com/url?sa=i&url=https%3A%2F%2Fsanctifiedart.org%2Fthose-who-dream-advent-bundle-year-b&psig=AOvVaw12bIqhuUxknJSLoO7rybEp&ust=1645976969446000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKjejJncnfYCFQAAAAAdAAAAABAE")
+
+
+        </img>
+    </head>
+    <body>
+
+        <h1>Yo!! Welcome to Dream's page</h1>
+
+    </body>
+</html>
+
+<style>
+    body 
+    
+</style>
---
+++ .history/test_20220226230926.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Welcome to Page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <h1>Yo!! Welcome to Dream's page</h1>
+    
+    <img style="    background-size: cover;
+ src='https://64.media.tumblr.com/ed944a6a889aee8d67ba53119011ade6/1abc53401f83a00e-7c/s1280x1920/59d9271d0acdeeedb5c358dc04f3e408f5d09dd3.gifv' id='selector'/>
+
+
+</body>
+
+</html>
