65217f8aa6a37f486e5dd2e350890300ff69b3a6
Ryuukaiser <gauvu107@gmail.com>
2022-04-05 21:08:24 +0700

    làm thêm xoá sửa user

--- resources/views/admin/food_detailFood.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>Chi tiết</h1>
-
-<h2>{{ $food->FoodName }}</h2>
-<p>{{ $food->FoodPrice }}</p>
-<p>{{ $food->FoodCategoryName }}</p>
-<p>{{ $food->FoodCoverPhoto }}</p>
-<div>{!! $Mota !!}</div>
