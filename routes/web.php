45149e3d67446aadffbcf68d4100b30761bf1952
Slime <guenvesley@gmail.com>
2022-04-06 09:12:12 +0700

    revert Quynh

--- routes/web.php
+++ routes/web.php
@@ -14,30 +14,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/home','App\Http\Controllers\HomeController@getHome');
 Route::post('/home','App\Http\Controllers\HomeController@postLogin');
-<<<<<<< HEAD
-Route::get('/menu','App\Http\Controllers\HomeController@getAll');
-Route::get('/menu/{idcatergory}','App\Http\Controllers\HomeController@getMenu');
-//login
-Route::post('login', 'App\Http\Controllers\HomeController@postLogin');
-
-Route::get('/admin', 'App\Http\Controllers\AdminController@index')->name('Admin');
-//thêm - sửa - xoá admin
-//thêm http://localhost:8000/admin/food/create/
-Route::get('/admin/food/create', 'App\Http\Controllers\Admin\AdminFoodsController@create');
-Route::post('/admin/food/store', 'App\Http\Controllers\Admin\AdminFoodsController@store');
-//Index http://localhost:8000/admin/food/
-Route::get('/admin/food', 'App\Http\Controllers\Admin\AdminFoodsController@index');
-
-//Edit http://localhost:8000/admin/food/
-Route::get('/admin/news/edit/{FoodCode}', 'App\Http\Controllers\Admin\AdminFoodsController@edit');
-Route::PATCH('/admin/news/edit/{FoodCode}', 'App\Http\Controllers\Admin\AdminFoodsController>@update');
-//Detail
-Route::get('/admin/news/{FoodCode}', 'App\Http\Controllers\Admin\AdminFoodsController@show');
-
-Route::DELETE('/admin/news/delete{FoodCode', 'App\Http\Controllers\Admin\AdminFoodsController@destroy');
-//register
-Route::post('register', 'App\Http\Controllers\HomeController@postRegister');
-=======
 
 //thêm - sửa - xoá admin
 Route::get('/admin/foodcatergory','App\Http\Controllers\Admin\FCController@index');
@@ -47,4 +23,3 @@ Route::get('/admin/drinkcatergory','App\Http\Controllers\Admin\DCController@inde
 Route::get('/admin/staff','App\Http\Controllers\Admin\StaffController@index');
 Route::get('/admin/user','App\Http\Controllers\Admin\UserController@index');
 
->>>>>>> 5175b9336a011ec76b7df2499d1004326d558502
